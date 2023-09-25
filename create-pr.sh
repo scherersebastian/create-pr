899dbbe59cbec70acba1da493339456adfbe7430
Sebastian Scherer <59142915+scherersebastian@users.noreply.github.com>
2023-09-25 21:37:43 +0200

    Update create-pr.sh

--- create-pr.sh
+++ create-pr.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Konstanten
-REPO_FULL_NAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
-
 # Eingabevariablen aus action.yml
 BASE_BRANCH="$INPUT_BASE"
 HEAD_BRANCH="$INPUT_HEAD"
@@ -11,15 +8,4 @@ PR_TITLE="$INPUT_TITLE"
 PR_BODY="$INPUT_BODY"
 
 # Erstellt einen PR
-PR_JSON=$(printf '{"title":"%s", "head":"%s", "base":"%s", "body":"%s"}' "$PR_TITLE" "$HEAD_BRANCH" "$BASE_BRANCH" "$PR_BODY")
-PR_RESPONSE=$(curl -s -X POST -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" -d "$PR_JSON" "https://api.github.com/repos/$REPO_FULL_NAME/pulls")
-
-# Überprüft, ob PR erfolgreich erstellt wurde
-if echo "$PR_RESPONSE" | jq -e .errors > /dev/null; then
-  echo "Fehler beim Erstellen des PRs:"
-  echo "$PR_RESPONSE" | jq .errors
-  exit 1
-fi
-
-echo "PR erfolgreich erstellt:"
-echo "$PR_RESPONSE" | jq .html_url
+gh pr create --base "$BASE_BRANCH" --head "$HEAD_BRANCH" --title "$PR_TITLE" --body "$PR_BODY"
