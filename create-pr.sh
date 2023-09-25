c1f27056bc91679878adc904b3e6719247bb0bd1
Sebastian Scherer <59142915+scherersebastian@users.noreply.github.com>
2023-09-25 21:58:06 +0200

    Update create-pr.sh

--- create-pr.sh
+++ create-pr.sh
@@ -7,5 +7,7 @@ HEAD_BRANCH="$INPUT_HEAD"
 PR_TITLE="$INPUT_TITLE"
 PR_BODY="$INPUT_BODY"
 
+echo "Debug: INPUT_TITLE = $INPUT_TITLE"
+
 # Erstellt einen PR
 gh pr create --base "$BASE_BRANCH" --head "$HEAD_BRANCH" --title "$PR_TITLE" --body "$PR_BODY"
